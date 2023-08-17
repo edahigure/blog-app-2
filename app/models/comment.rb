5a1a5b47187e025f3a9ba080da842f325cb2f58e
Edahí Antonio Gutiérrez Reyes <edahigure@gmail.com>
2023-08-17 17:41:26 -0600

    remove unnecesary class_name

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,6 @@
 class Comment < ApplicationRecord
   belongs_to :author, foreign_key: :author_id, class_name: 'User'
-  belongs_to :post, foreign_key: :post_id, class_name: 'Post'
+  belongs_to :post, foreign_key: :post_id
 
   after_create :update_comments_counter
 
--- app/models/like.rb
+++ app/models/like.rb
@@ -3,7 +3,7 @@ class Like < ApplicationRecord
   belongs_to :post
 
   belongs_to :author, foreign_key: :author_id, class_name: 'User'
-  belongs_to :post, foreign_key: :post_id, class_name: 'Post'
+  belongs_to :post, foreign_key: :post_id
 
   after_create :update_likes_counter
 
