0c1aea6b6ed486d8d7ac3f131074a90276d2550f
unknown <teddytiyi@gmail.com>
2023-09-02 16:49:31 +0200

    Create an API endpoint to list all posts for a user.

---
+++ app/controllers/api/v1/posts_controller.rb
@@ -0,0 +1,13 @@
+class Api::V1::PostsController < ApplicationController 
+
+    def index   
+     @author_id = params[:user_id]
+    @posts = Post.where( author_id: @author_id  )
+      
+        if @posts
+          render json: {status: "SUCCESS", message: "Fetched all the friends successfully", data: @posts}, status: :ok
+        else
+          render json: @posts.errors, status: :bad_request
+        end
+      end
+end
