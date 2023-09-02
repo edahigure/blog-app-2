62df82f6f76c2910c295dec66c63d2d54030f383
unknown <teddytiyi@gmail.com>
2023-09-02 18:23:17 +0200

    Create an API endpoint to add a comment to a post.

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -4,8 +4,25 @@ class Api::V1::CommentsController < ApplicationController
     render json: @post.comments
   end
 
-  def show
-
+  def create
+    post_id = params[:id]
+    user_id = params[:user_id]
+    user = User.find(user_id)
+    post = Post.find(post_id)
+    comments =Comments.new(comments_params)
+    comments.author = User.find(user_id)
+    comment.post = Post.find(post_id)
+  
+    if comments.save
+      render json: {status: "SUCCESS", message: "comments was created successfully!", data: comments}, status: :created
+    else
+      render json: comments.errors, status: :unprocessable_entity
+    end
   end
-
+  
+  private
+  
+  def comments_params
+    params.require(:comments).permit(:text)
+  end
 end
